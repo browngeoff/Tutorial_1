ba778d64ff86bc79fb90895244ff00d4009bf20b
browngeoff <browng@accesscable.net>
2013-02-25 09:33:20 -0400

    My first commit

---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+
+
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private IKernel ninjectKernal;
+
+        public NinjectControllerFactory()
+        {
+            ninjectKernal = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null
+                ? null
+                : (IController)ninjectKernal.Get(controllerType);
+
+        }
+
+        private void AddBindings()
+        {
+            ninjectKernal.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+    }
+}
